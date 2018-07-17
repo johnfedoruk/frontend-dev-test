4f081ac5201acb5ea989cc8f656d82d61860e0ad
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-07-16 22:03:07 -0500

    Providing AuthService to AuthModule

--- src/app/apps/auth/auth.module.ts
+++ src/app/apps/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthService } from './services/auth.service';
+
 @NgModule({
   imports: [
     CommonModule
+  ],
+  providers: [
+    AuthService
   ],
   declarations: []
 })
