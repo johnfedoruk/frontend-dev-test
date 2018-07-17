910f7fb1b7c77b4b7165795697b7343e33b981a2
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-07-16 22:34:37 -0500

    Setup routing to LoginComponent and LogoutComponent in AuthViewModule

--- src/app/apps/auth-view/auth-view.module.ts
+++ src/app/apps/auth-view/auth-view.module.ts
@@ -1,13 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AuthViewComponent } from './auth-view.component';
 import { LoginComponent } from './pages/login/login.component';
 import { LogoutComponent } from './pages/logout/logout.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthViewComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'logout',
+        component: LogoutComponent
+      }
+    ]
+  }
+];
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [AuthViewComponent, LoginComponent, LogoutComponent]
+  declarations: [
+    AuthViewComponent,
+    LoginComponent,
+    LogoutComponent
+  ]
 })
 export class AuthViewModule { }
