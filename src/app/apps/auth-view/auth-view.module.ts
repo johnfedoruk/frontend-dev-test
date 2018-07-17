ef7e77366129f7fe680b436ef7abc1891de457f7
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-07-16 22:24:39 -0500

    Added the LoginComponent page to AuthViewModule

--- src/app/apps/auth-view/auth-view.module.ts
+++ src/app/apps/auth-view/auth-view.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthViewComponent } from './auth-view.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AuthViewComponent]
+  declarations: [AuthViewComponent, LoginComponent]
 })
 export class AuthViewModule { }
