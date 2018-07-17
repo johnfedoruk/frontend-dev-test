3a717327831c8bc802be93e1cadd56beaa2796f5
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-07-16 22:25:44 -0500

    Added a LogoutComponent page to AuthViewModule

--- src/app/apps/auth-view/auth-view.module.ts
+++ src/app/apps/auth-view/auth-view.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthViewComponent } from './auth-view.component';
 import { LoginComponent } from './pages/login/login.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AuthViewComponent, LoginComponent]
+  declarations: [AuthViewComponent, LoginComponent, LogoutComponent]
 })
 export class AuthViewModule { }
