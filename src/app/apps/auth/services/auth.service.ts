288d684ceee234c233ea7b8e56b5c345368015af
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-07-17 00:11:32 -0500

    Implemented AuthService

--- src/app/apps/auth/services/auth.service.ts
+++ src/app/apps/auth/services/auth.service.ts
@@ -1,11 +1,94 @@
 import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Account } from 'common';
+
 import { AuthConfig } from '../auth.config';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+const TOKEN = 'jwt';
+const ACCOUNT = 'account';
 
 @Injectable()
 export class AuthService {
 
+  public profile$: Subject<Account> = new BehaviorSubject<Account>(undefined);
+
   constructor(
-    @Inject('config') private config: AuthConfig
-  ) { }
+    @Inject('config') private config: AuthConfig,
+    private http: HttpClient
+  ) {
+    if (this.token && this.account) {
+      this.profile$.next(this.account);
+    }
+  }
+
+  public async login(username: string, password: string): Promise<void> {
+    return new Promise<void>(
+      async (resolve, reject) => {
+        let token: string;
+        let account: Account;
+        try {
+          token = await this.getToken(username, password);
+          account = await this.getAccount(token);
+          this.token = token;
+          this.account = account;
+          this.profile$.next(this.account);
+          console.log(this.account, this.token);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      }
+    );
+  }
+
+  public logout(): void {
+    window.sessionStorage.clear();
+  }
+
+  private async getToken(username: string, password: string): Promise<any> {
+    const url: string = this.config.login_url;
+    const headers: HttpHeaders = new HttpHeaders({
+      'Authorization': `Basic ${btoa(`${username}:${password}`)}`
+    });
+    const responseType: any = 'text';
+    const config = {
+      responseType,
+      headers
+    };
+    return await this.http.get<string>(url, config).toPromise<string>();
+  }
+
+  private async getAccount(token: string): Promise<Account> {
+    const url: string = this.config.me_url;
+    const headers: HttpHeaders = new HttpHeaders({
+      'Authorization': `JWT ${token}`
+    });
+    const responseType: any = 'json';
+    const config = {
+      responseType,
+      headers
+    };
+    return await this.http.get<Account>(url, config).toPromise<Account>();
+  }
+
+  public set token(token: string) {
+    window.sessionStorage.setItem(TOKEN, token);
+  }
+
+  public get token(): string {
+    return window.sessionStorage.getItem(TOKEN);
+  }
+
+  public set account(account: Account) {
+    window.sessionStorage.setItem(ACCOUNT, JSON.stringify(account));
+  }
+
+  public get account(): Account {
+    return JSON.parse(window.sessionStorage.getItem(ACCOUNT));
+  }
 
 }
