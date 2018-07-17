50e46d98e7ba0a28d8bebf926ad0368e08c68f4f
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-07-17 00:23:27 -0500

    Defined LogoutComponent's basic logic

--- src/app/apps/auth-view/pages/logout/logout.component.ts
+++ src/app/apps/auth-view/pages/logout/logout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.auth.logout();
   }
 
 }
